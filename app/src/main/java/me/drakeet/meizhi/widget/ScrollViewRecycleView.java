1911f9b7a2c67ba1c85c0ed4f984b67c2948ffeb
AFinalStone <602392033@qq.com>
2017-03-13 18:06:11 +0800

    Initial commit

---
+++ app/src/main/java/me/drakeet/meizhi/widget/ScrollViewRecycleView.java
@@ -0,0 +1,44 @@
+package me.drakeet.meizhi.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ScrollView;
+
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+public class ScrollViewRecycleView extends RecyclerView{
+
+    public ScrollViewRecycleView(Context context) {
+        super(context);
+    }
+
+    public ScrollViewRecycleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollViewRecycleView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if (h > height)
+                height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
